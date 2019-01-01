4ab1beb924f1bdb22f8d70851cde05fd20eabb82
Cold Zebra <zachdbray@gmail.com>
2019-01-01 13:57:08 -0600

    Begun app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  //Needed Variables
+  var correctAnswers;
+  var wrongAnswers;
+  var unansweredquestions;
+  var timeRemaining;
+
+//Displays quiz content and starts countdown when user clicks start
+  $("#startButton").on("click", gameInit)
+
+
+
+}
